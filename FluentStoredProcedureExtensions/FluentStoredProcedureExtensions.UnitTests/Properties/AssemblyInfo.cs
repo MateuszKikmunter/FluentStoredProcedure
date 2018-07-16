5fd90eaa3bb615944c9be28568129415b8f9cf6c
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-07-16 14:50:27 +0100

    init - added basic project structure

---
+++ FluentStoredProcedureExtensions/FluentStoredProcedureExtensions.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FluentStoredProcedureExtensions.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("FluentStoredProcedureExtensions.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("21ecb5a2-892b-4377-9e4d-79fab361a98b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
