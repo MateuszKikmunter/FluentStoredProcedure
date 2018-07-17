bca0f5a3d6c1258836350d7fb771807c555d1402
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-07-17 14:06:55 +0100

    swapped unit tests project to .NET Core version

---
+++ FluentStoredProcedureExtensions/FluentStoredProcedureExtensions.UnitTests/DataServicesTests/SqlParameterFactoryTests.cs
@@ -0,0 +1,11 @@
+﻿using FluentStoredProcedureExtensions.Core.Abstract;
+using NUnit.Framework;
+
+namespace FluentStoredProcedureExtensions.UnitTests.DataServicesTests
+{
+    [TestFixture]
+    public class SqlParameterFactoryTests
+    {
+        private ISqlParameterFactory _sqlParameterFactory;
+    }
+}
--- FluentStoredProcedureExtensions/FluentStoredProcedureExtensions/Abstract/IStoredProcedure.cs
+++ FluentStoredProcedureExtensions/FluentStoredProcedureExtensions/Abstract/IStoredProcedure.cs
@@ -7,8 +7,8 @@ namespace FluentStoredProcedureExtensions.Core.Abstract
     public interface IStoredProcedure
     {
         string StoredProcedureText { get; }
-        void SetStoredProcedureText(string commandText);
         IList<SqlParameter> SqlParametersCollection { get; }
+        void SetStoredProcedureText(string commandText);
         IStoredProcedure WithSqlParam(string parameterName, Action<SqlParameter> configureParameter = null);
         IStoredProcedure WithSqlParam(string parameterName, object paramValue, Action<SqlParameter> configureParam = null);
         IStoredProcedure WithUserDefinedDataTableSqlParam<T>(string parameterName, IList<T> paramValue, Action<SqlParameter> configureParam = null);
